483d5320ca52718fd3ed49765c76d0ff6ebbe6bf
Lionel Schiepers <lionel_schiepers@hotmail.com>
2025-09-13 13:00:11 +0200

    add exception details

--- MinimalAPIService/Program.cs
+++ MinimalAPIService/Program.cs
@@ -1,11 +1,10 @@
-using Microsoft.Extensions.Configuration;
 using MinimalAPIService.SimulationService;
 using Scalar.AspNetCore;
 using Serilog;
-using Serilog.Formatting.Compact;
-using Serilog.Sinks.SystemConsole.Themes;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddProblemDetails();
 
 builder.Services.AddOpenApi();
 
@@ -19,6 +18,26 @@ builder.Services.AddSerilog((sp, config) =>
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+{
+    app.UseExceptionHandler(exceptionHandlerApp =>
+    {
+        exceptionHandlerApp.Run(async httpContext =>
+        {
+            var pds = httpContext.RequestServices.GetService<IProblemDetailsService>();
+            if (pds == null || !await pds.TryWriteAsync(new() { HttpContext = httpContext }))
+                await httpContext.Response.WriteAsync("Fallback: An error occurred.");
+        });
+    });
+}
+
+app.UseStatusCodePages(async statusCodeContext
+    => await Results
+        .Problem(statusCode: statusCodeContext.HttpContext.Response.StatusCode)
+        .ExecuteAsync(statusCodeContext.HttpContext));
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
@@ -31,6 +50,18 @@ if (app.Environment.IsDevelopment())
         .WithDarkMode();
     }).AllowAnonymous();
 }
+
+#if DEBUG
+app.MapGet("/exception", () =>
+{
+    throw new InvalidOperationException("Sample Exception");
+});
+
+app.MapGet("/status500", () =>
+{
+    return Results.StatusCode(500);
+});
+#endif
 
 if (!app.Environment.IsDevelopment())
 {
